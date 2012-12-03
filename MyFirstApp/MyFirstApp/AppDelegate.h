d76f558a3a35721645c719d9a7629ec799ba814b
davidsph <daviddjy@163.com>
2012-12-03 09:56:47 +0800

    hh

---
+++ MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by LIAN YOU on 11/27/12.
+//  Copyright (c) 2012 LIAN YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFirstApp/MyFirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApp
+//
+//  Created by LIAN YOU on 11/27/12.
+//  Copyright (c) 2012 LIAN YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(retain) NSString *name;
+@end
+
